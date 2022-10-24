d320126099f27b3dc2bcb18bfdfafd40beec5ee1
Alisher <baisaganov99@gmail.com>
2022-10-25 00:25:07 +0600

    Add features to work with books

--- src/main/java/kz/alisher/library/controllers/BookController.java
+++ src/main/java/kz/alisher/library/controllers/BookController.java
@@ -1,9 +1,11 @@
 package kz.alisher.library.controllers;
 
 import kz.alisher.library.dao.BookDAO;
+import kz.alisher.library.models.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Component
 @RequestMapping("/books")
@@ -14,4 +16,45 @@ public class BookController {
     public BookController(BookDAO bookDAO) {
         this.bookDAO = bookDAO;
     }
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("books", bookDAO.index());
+        return "books/index";
+    }
+
+    @GetMapping("/new")
+    public String addBook(@ModelAttribute("book") Book book){
+        return "books/new";
+    }
+    @PostMapping()
+    public String saveBook(@ModelAttribute("book") Book book){
+        bookDAO.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}")
+    public String showBook(@PathVariable("id") int id, Model model) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/show";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editBook(@PathVariable("id") int id, Model model){
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String editing(@PathVariable("id") int id, @ModelAttribute("book") Book book){
+        bookDAO.update(id, book);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteBook(@PathVariable("id") int id){
+        bookDAO.delete(id);
+        return "redirect:/books";
+    }
+
 }
